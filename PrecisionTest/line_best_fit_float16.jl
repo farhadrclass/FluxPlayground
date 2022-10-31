62a95123f2c1898569853b2d7117e3c857eeec6b
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2022-10-31 17:30:17 -0400

    Train with float16

--- PrecisionTest/line_best_fit_float16.jl
+++ PrecisionTest/line_best_fit_float16.jl
@@ -22,21 +22,28 @@ x_train, x_test = hcat(0.0:5.0...), hcat(6.0:10...)
 y_train_true, y_test_true = actual.(x_train), actual.(x_test)
 y_train, y_test = deepcopy(y_train_true),deepcopy(y_test_true)
 
-# #noise added
-# map!(vi->vi+(rand(1:100)/1000),y_train,y_train) 
-# map!(vi->vi+(rand(1:100)/1000),y_test,y_test) 
+println("Type of the data is " , typeof(x_train)," y is ",typeof(y_train))
+
+
+x_train, x_test = Float16.(x_train),Float16.(x_test)
+y_train, y_test = Float16.(y_train), Float16.(y_test)
+
+println("Type of the data is " , typeof(x_train)," y is ",typeof(y_train))
 
 
 # model 
 ## 
-model = Dense(1 => 1)
-# here I will test Model as float16
-
-println( model.weight)
-println(model.bias)
 
 # I can name it predict or keep it model
-predict = Dense(1 => 1)
+# predict = Dense(1 => 1)
+
+predict = Dense(rand(Float16,1,1),true) # true is for bias
+
+# The dimensions of these model parameters depend on the number 
+# of inputs and outputs. Since models can have hundreds of inputs and several layers, it helps to have a function 
+# to collect the parameters into the data structure Flux expects:
+parameters = Flux.params(predict)
+println("type of the parameters", parameters) #TODO fix this and print the type
 
 # The loss function  Here is MSE
 loss(x, y) = Flux.Losses.mse(predict(x), y);
@@ -47,10 +54,7 @@ opt = Descent()
 # data 
 data = [(x_train, y_train)]
 
-# The dimensions of these model parameters depend on the number 
-# of inputs and outputs. Since models can have hundreds of inputs and several layers, it helps to have a function 
-# to collect the parameters into the data structure Flux expects:
-parameters = Flux.params(predict)
+
 
 print("LOSS: ")
 println(loss(x_train, y_train))
--- README.md
+++ README.md
@@ -7,8 +7,9 @@
 
 
 ## Todo
-- [ ] Train simple model on Flux
-- [ ] Try it in Float32 and Float64
+- [x] Train simple model on Flux
+- [x] Try it in Float32 and Float64, 
+- [x] Float16
 - [ ] Move to GPU
 - [ ] Data loader
 - [ ] MNIST data 
@@ -35,3 +36,17 @@
     Params([Float32[0.9066542], Float32[0.0]])
     ```
     These are the parameters Flux will change, one step at a time, to improve predictions. At each step, the contents of this Params object changes too, since it is just a collection of references to the mutable arrays inside the model:
+ - To change the type 
+    ```
+    
+    predict = Dense(rand(Float16,1,1),true) # true is for bias
+
+    # The dimensions of these model parameters depend on the number 
+    # of inputs and outputs. Since models can have hundreds of inputs and several layers, it helps to have a function 
+    # to collect the parameters into the data structure Flux expects:
+    parameters = Flux.params(predict)
+    println("type of the parameters", parameters) #TODO fix this and print the type
+
+    # The loss function  Here is MSE
+    loss(x, y) = Flux.Losses.mse(predict(x), y);
+    ```
